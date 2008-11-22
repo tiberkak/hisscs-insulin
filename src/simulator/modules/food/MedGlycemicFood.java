5fe32d4ca1880666d44c43bb91e54fa2ee9a2fe3
nikolas.orlowski@gmx.de <nikolas.orlowski@gmx.de@816bef66-afc4-11dd-b6fe-af671ef85443>
2008-11-22 19:33:54 +0000

    First prototype of food simulation module <rcg>.

---
+++ src/simulator/modules/food/MedGlycemicFood.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package simulator.modules.food;
+
+/**
+ * @author rc
+ *
+ */
+public class MedGlycemicFood extends AbstractFood {
+
+	/**
+	 * @param ammount
+	 * @param time
+	 */
+	public MedGlycemicFood(int ammount, int time) {
+		super(ammount, time);
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see simulator.modules.food.AbstractFood#calculateGlucose(int)
+	 */
+	@Override
+	double calculateGlucose(int time) {
+		int localTime = time - this.time;
+		double quotient = localTime / 30.d;
+		return (quotient < 1) ? quotient : 2 - quotient;  
+	}
+
+}
