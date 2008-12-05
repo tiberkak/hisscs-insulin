485ff007136cd60952fee9e0bd595430d72a4cfc
nikolas.orlowski <nikolas.orlowski@gmx.de>
2008-12-05 12:13:30 +0000

    Edited by Solomon for graphical input

--- src/simulator/view/input/GraphicalInput.java
+++ src/simulator/view/input/GraphicalInput.java
@@ -3,7 +3,20 @@
  */
 package simulator.view.input;
 
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JToggleButton;
+
 import simulator.controller.AbstractController;
+import simulator.controller.ConcreteController;
+import simulator.modules.food.ChartDisplay;
+
 
 /**
  * @author rc
@@ -11,12 +24,69 @@ import simulator.controller.AbstractController;
  */
 public class GraphicalInput extends AbstractInput {
 
-	/**
-	 * @param controller2
-	 */
+
+	private ConcreteController controller;
+	private ChartDisplay chartDisplay;	
+	private boolean pause = false;
+	
 	public GraphicalInput(AbstractController controller2) {
-		super(controller2);
-		// TODO Auto-generated constructor stub
+		super(controller2);	
+		controller=new ConcreteController();
 	}
+	public GraphicalInput() {
+		controller=new ConcreteController();
+	}	
+	public void workLoop() throws IOException {
+		JFrame frame = new JFrame("Food control");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new GridLayout(0, 1, 2, 5));
+
+		JButton addHighButton = new JButton("Add high glycemic food");
+		frame.add(addHighButton);
+		addHighButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent aE) {
+				controller.addFood(AbstractController.FoodType.HIGH);
+			}
+		});
+
+		JButton addMedButton = new JButton("Add med glycemic food");
+		frame.add(addMedButton);
+		addMedButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent aE) {
+				controller.addFood(AbstractController.FoodType.MED);				
+			}
+		});
+
+		JButton addLowButton = new JButton("Add low glycemic food");
+		frame.add(addLowButton);
+		addLowButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent aE) {
+				controller.addFood(AbstractController.FoodType.LOW);				
+			}
+		});
+
+		JToggleButton toggleChartButton = new JToggleButton("Hide chart");
+		frame.add(toggleChartButton);
+		toggleChartButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent aE) {
+				if (chartDisplay.isVisible()) {
+					chartDisplay.setVisible(false);
+				} else {
+					chartDisplay.setVisible(true);
+				}
+			}
+		});
+
+		JToggleButton togglePauseButton = new JToggleButton("Pause");
+		frame.add(togglePauseButton);
+		togglePauseButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent aE) {
+				controller.setPause(pause);
+			}
+		});
+
+		frame.pack();
+		frame.setVisible(true);
+	}	
 
 }
